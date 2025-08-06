5c2b2bb5705b92e01268d15713a9b4f4bef9ccab
Volodymyr <sainchukvova2104@gmail.com>
2025-08-07 00:09:54 +0300

    fix bugs n +2

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,18 +2,16 @@ import { fetchNoteById } from "@/lib/api";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 
-type Props = {
-  params: { id: string };
-};
+type Params = Promise<{ id: string }>;
 
-export default async function NoteDetails({ params }: Props) {
-  const noteId = params.id;
+export default async function NoteDetails({ params }: { params: Params }) {
+  const { id } = await params;
 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["note", noteId],
-    queryFn: () => fetchNoteById(noteId),
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
   });
 
   return (
