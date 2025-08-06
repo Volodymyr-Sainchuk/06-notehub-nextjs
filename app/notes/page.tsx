d5c17023c4f922d85d71cb1f18447f01fbab2f54
Volodymyr <sainchukvova2104@gmail.com>
2025-08-06 13:32:30 +0300

    fix bugs 4

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,6 +3,7 @@ import Notes from "./Notes.client";
 
 export default async function NotesPage() {
   const initialData = await fetchNotes({ query: "", page: 1, perPage: 12 });
+  console.log(initialData);
 
   return <Notes initialData={initialData} />;
 }
